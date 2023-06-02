b6b00b501246e27768c3c976f6b1ef025a9b40b9
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-02 14:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const container = document.getElementsByClassName("container")[0] ;
+function showElement() {
+	container.innerHTML = '' ;
+	const div = document.createElement("div")
+		div.id = "line" ;
+	div.className = "line" ;
+	container.append(div);
+}
