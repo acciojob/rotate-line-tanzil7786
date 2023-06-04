c82af90d437fa5ace305c53481db71d581b95d06
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 13:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let div=document.createElement("div");
-div.id="line"
-div.style.position="absolute"
-div.style.width="200px"
-div.style.height="2px"
-div.style.backgroundColor="black"
-div.style.animation = "rotateAnimation 2s infinite";
-let body=document.getElementsByTagName("body")[0];
-body.append(div);
+let container=document.createElement('div');
+container.className="container";
+let line=document.createElement('div');
+line.id="line";
+
+container.append(line);
+
+document.body.append(container);
