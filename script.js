7d1941a2d3148d0c774fc2cf032a698903830b9c
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 13:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const container = document.getElementsByClassName("container")[0] ;
-function showElement() {
-	container.innerHTML = '' ;
-	const div = document.createElement("div")
-		div.id = "line" ;
-	div.className = "line" ;
-	container.append(div);
-}
+let div=document.createElement("div");
+div.id="line"
+div.style.position="absolute"
+div.style.width="200px"
+div.style.height="2px"
+div.style.backgroundColor="black"
+div.style.animation = "rotateAnimation 2s infinite";
+let body=document.getElementsByTagName("body")[0];
+body.append(div);
